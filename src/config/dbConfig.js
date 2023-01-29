09c27afe6802721dd4856aca2e0e6cf30ba8b174
yangchen55 <tseyang986@gmail.com>
2023-01-30 10:29:16 +1100

    djf

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
         // const connStr = 'mongodb://localhost:27017/transaction'
         // mongodb+srv://transaction:<password>@cluster0.k4qvjoj.mongodb.net/?retryWrites=true&w=majority
         const conn = mongoose.connect(process.env.MONGO_CLIENT);
-        conn ? console.log("mongo connected"):console.log("unable to connect Mongo");;
+        conn ? console.log("mongo connected") : console.log("unable to connect Mongo");;
     } catch (error){
         console.log(error);
     }
